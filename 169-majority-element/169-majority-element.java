82fcab2dba670c49378413d9af4996acb7aa0636
Saumya Tiwari <taru.saumya@gmail.com>
2022-03-27 16:42:06 +0530

    Time: 3 ms (60.63%), Space: 56.2 MB (43.54%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+        
+        for(int n: nums){
+            if(count == 0)
+                candidate = n;
+            
+            count += (n == candidate) ? 1 : -1;
+        }
+        
+        return candidate;
+        
+    }
+}
