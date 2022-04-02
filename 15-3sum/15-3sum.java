de122c7f70596398963856cedb7d4a113835c802
Saumya Tiwari <taru.saumya@gmail.com>
2022-04-02 23:16:00 +0530

    Time: 56 ms (33.15%), Space: 65.4 MB (24.22%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -3,18 +3,17 @@ class Solution {
         
         Arrays.sort(nums);
         
-        List<List<Integer>> res = new ArrayList<>();
+        Set<List<Integer>> res = new HashSet<>();
         
         for(int i = 0; i < nums.length && nums[i] <= 0; i++){
-            if(i == 0 || nums[i - 1] != nums[i])
-                twoSum(i, res, nums); 
+           twoSum(i, res, nums); 
         }
             
-          return res;
+          return new ArrayList(res);
         
     }
     
-    void twoSum(int i, List<List<Integer>> res, int[] nums){
+    void twoSum(int i, Set<List<Integer>> res, int[] nums){
         int lo = i+ 1;
         int hi = nums.length - 1;
         
@@ -24,13 +23,13 @@ class Solution {
             int sum = nums[i] + nums[lo] + nums[hi];
             
             if(sum < 0)
-                ++lo;
+                lo++;
             else if(sum > 0)
-                --hi;
+                hi--;
             else{
                 res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
                 while(lo < hi && nums[lo] == nums[lo - 1])
-                    ++lo;
+                lo++;
             }
             
         }
